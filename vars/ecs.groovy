500965bbef03afafb2beb98e1373f1379f4631e0
arunodhayamsam <100224916+arunodhayamsam@users.noreply.github.com>
2023-08-29 13:28:24 +0530

    Updated ecs.groovy

---
+++ vars/ecs.groovy
@@ -0,0 +1,15 @@
+def dockerBuildAndPush(){
+    sh 'rm -f ~/.dockercfg ~/.docker/config.json || true'
+            docker.withRegistry('http://790285888667.dkr.ecr.us-east-1.amazonaws.com/node-app', 'ecr:us-east-1:aws') {
+            def customImage = docker.build("node-app:${env.BUILD_ID}")
+            customImage.push()  
+    }        
+}
+
+def updateTaskdef(){
+
+}
+
+def deployEcs(){
+
+}
