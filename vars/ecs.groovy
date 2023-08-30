c15ab72b53cd40f8bbcf48d86690ecc3ad77bc40
arunodhayamsam <100224916+arunodhayamsam@users.noreply.github.com>
2023-08-30 12:18:47 +0530

    Updated the function jenkinsfile1

--- vars/ecs.groovy
+++ vars/ecs.groovy
@@ -1,14 +1,18 @@
 def dockerBuildAndPush(){
     sh 'rm -f ~/.dockercfg ~/.docker/config.json || true'
-            docker.withRegistry('https://707072725274.dkr.ecr.us-east-1.amazonaws.com', 'ecr:us-east-1:aws') {
-            def customImage = docker.build("node-app:${env.BUILD_ID}")
-            customImage.push()                        
-         }        
+        //     docker.withRegistry('https://707072725274.dkr.ecr.us-east-1.amazonaws.com', 'ecr:us-east-1:aws') {
+        //     def customImage = docker.build("node-app:${env.BUILD_ID}")
+        //     customImage.push()                        
+        //  }    
+        withDockerRegistry(credentialsId: 'ecr:us-east-1:aws', url: 'https://707072725274.dkr.ecr.us-east-1.amazonaws.com') {
+             def customImage = docker.build("node-app:${env.BUILD_ID}")
+             customImage.push()
+}    
 }
 
 def updateTaskdef(){
     // This step should not normally be used in your script. Consult the inline help for details.
-withDockerRegistry(credentialsId: 'ecr:us-east-1:aws', url: '707072725274.dkr.ecr.us-east-1.amazonaws.com/node-app') {
+withDockerRegistry(credentialsId: 'ecr:us-east-1:aws', url: 'https://707072725274.dkr.ecr.us-east-1.amazonaws.com') {
     def customImage = docker.build("node-app:${env.BUILD_ID}")
     customImage.push()
 }
