d402f9bd980484bb929b6d48f44255f56c8ce6f6
arunodhayamsam <100224916+arunodhayamsam@users.noreply.github.com>
2023-08-30 11:44:18 +0530

    Updated function

--- vars/ecs.groovy
+++ vars/ecs.groovy
@@ -1,15 +1,15 @@
 def dockerBuildAndPush(){
     sh 'rm -f ~/.dockercfg ~/.docker/config.json || true'
-            // docker.withRegistry('https://790285888667.dkr.ecr.us-east-1.amazonaws.com/node-app', 'ecr:us-east-1:aws') {
-            // def customImage = docker.build("node-app:${env.BUILD_ID}")
-            // customImage.push()  
+            docker.withRegistry('https://707072725274.dkr.ecr.us-east-1.amazonaws.com', 'ecr:us-east-1:aws') {
+            def customImage = docker.build("node-app:${env.BUILD_ID}")
+            customImage.push()  
 
-            withDockerRegistry(credentialsId: 'ecr:us-east-1:aws', url: 'https://707072725274.dkr.ecr.us-east-1.amazonaws.com/node-app') {
-              def customImage = docker.build("node-app:${env.BUILD_ID}")
-              customImage.push()
-       }
+    //         withDockerRegistry(credentialsId: 'ecr:us-east-1:aws', url: 'https://707072725274.dkr.ecr.us-east-1.amazonaws.com/node-app') {
+    //           def customImage = docker.build("node-app:${env.BUILD_ID}")
+    //           customImage.push()
+    //    }
     }        
-
+}
 
 def updateTaskdef(){
 
